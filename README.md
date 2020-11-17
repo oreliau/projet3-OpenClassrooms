0e93918866523e7d53ebc08991b2489474723df4
Oreliau KOUMEDZRO <oreliauk@gmail.com>
2020-11-17 20:26:40 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # projet3-OpenClassrooms
+
+## Contribution
+[10 modern layouts in 1 line of CSS given by Google](https://www.youtube.com/watch?v=qm0IfG1GyZU&t=888s)
+[Idea for main page and images](https://www.youtube.com/watch?v=TawH-AqHTXc)
+[parallax on planning page](https://www.w3schools.com/howto/howto_css_parallax.asp)
+
+Image de cinéma en plein air © FCPA Villette © Alain Goustard
---
+++ main.js
@@ -0,0 +1,33 @@
+let background = document.getElementById('bg');
+let moon = document.getElementById('moon');
+let mountain = document.getElementById('mountain');
+let road = document.getElementById('road');
+let text = document.getElementById('text');
+
+function imageStartAt(imageNode, timeOut){
+    setTimeout(function(){ imageNode.querySelector('img').classList.remove('disp-no'); }, timeOut);
+}
+
+window.addEventListener('scroll', function(){
+    let scrollValue = window.scrollY;
+    let offsetTop = document.querySelector('.main-content').offsetTop
+    background.style.top = scrollValue * 0.5 + "px";
+    moon.style.left = -scrollValue * 0.5 + "px";
+    mountain.style.top = -scrollValue * 0.15 + "px";
+    road.style.top = scrollValue * 0.15 + "px";
+    text.style.top = scrollValue * 1 + "px";
+
+    if(scrollValue > (offsetTop - (offsetTop/2))){
+        const imagesOngletFirst = document.querySelector('#image-onglet1');
+        const imagesOngletSecond = document.querySelector('#image-onglet2');
+        const imagesOngletThird = document.querySelector('#image-onglet3');
+        const imagesOngletFourth = document.querySelector('#image-onglet4');
+        const textDescription = document.querySelector('.description-association');
+
+        imageStartAt(imagesOngletFirst, 250);
+        imageStartAt(imagesOngletSecond, 550);
+        imageStartAt(imagesOngletThird, 750);
+        imageStartAt(imagesOngletFourth, 500);
+        setTimeout(function(){ textDescription.classList.remove('disp-no'); }, 500);
+    }
+})
